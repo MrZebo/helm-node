ba70d08c1057cefbc1cc2990df3694fbdc39d219
Borys Hrebennikov <borys.hrebennikov@chatbotsstudio.com>
2021-01-10 19:13:55 +0200

    test commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('12Hello world21\n');
+  res.send('123Hello world321\n');
 });
 
 app.listen(PORT, HOST);
